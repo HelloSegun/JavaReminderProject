01727c43aee29e145f4e5ac5e42c48f526f76a6f
HelloSegun <segunkuye@hotmail.com>
2018-10-29 01:12:57 +0000

    ConditionalOperator, InstanceOfOperator, LoopControl 29102018

---
+++ src/InstanceOfOperator.java
@@ -0,0 +1,11 @@
+
+public class InstanceOfOperator {
+
+	public static void main (String [] args) {
+		String name = "James";
+		
+		// following will return true since name is type string
+		boolean result = name instanceof String;
+		System.out.println(result);
+	}
+}
---
+++ src/LoopControl.java
@@ -0,0 +1,13 @@
+
+public class LoopControl {
+
+	public static void main (String [] args) {
+		int x = 10;
+		
+		while (x <= 20) {
+			System.out.println("Value of x is : " + x);
+			x++;
+			System.out.println("\n");
+		}
+	}
+}
